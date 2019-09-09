3e3ef3ae551905f4ed675828895ad5556ba4de2a
Laura Schulz <lrs8810@gmail.com>
2019-09-08 18:17:38 -0600

    Add initialize methods and tests

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,8 +1,10 @@
 require './lib/activity'
 
 class Reunion
+  attr_reader :activities, :cohort_name
 
   def initialize(cohort_name)
     @cohort_name = cohort_name
+    @activities = []
   end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -10,4 +10,9 @@ class ReunionTest < Minitest::Test
   def test_it_exists
     assert_instance_of Reunion, @reunion
   end
+
+  def test_initialize
+    assert_equal [], @reunion.activities
+    assert_equal "1406 BE", @reunion.cohort_name
+  end
 end
